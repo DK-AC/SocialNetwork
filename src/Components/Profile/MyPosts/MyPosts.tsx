b3da73b0019e3ca7c7620481ae692cfa47fb6f38
DK-AC <denkacaj@gmail.com>
2021-08-19 13:54:08 +0300

    moved all textarea logic into a separate component

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,7 +1,8 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import React, {useState} from "react";
 import styles from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
 import {PostsType} from "../../../redux/profileReducer";
+import {TextareaForMyPosts} from "./Textarea";
 
 type MyPostsPropsType = {
     posts: Array<PostsType>
@@ -30,31 +31,21 @@ export function MyPosts(props: MyPostsPropsType) {
         props.updateNewPostText('')
     }
 
-    const onPostChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-        props.updateNewPostText(event.currentTarget.value)
-        serError(false)
-    }
-
-    const onKeyPress = (event: KeyboardEvent<HTMLTextAreaElement>) => {
-        if (event.key === 'Enter') onAddNewPost()
-    }
-
     //если в инпут ни чего не ввели выведу ошибку
     const errorMessage = error
         ? <div className={styles.errorText}>Post should not be empty!</div>
         : null
-
 
     return (
         <div className={styles.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea value={props.newPostText}
-                              placeholder={'enter your message'}
-                              onChange={onPostChange}
-                              onKeyPress={onKeyPress}
-                              className={error ? styles.error : ''}
+                    <TextareaForMyPosts error={error}
+                                        setError={serError}
+                                        newPostText={props.newPostText}
+                                        onAddNewPost={onAddNewPost}
+                                        updateNewPostText={props.updateNewPostText}
                     />
                 </div>
                 <div>
---
+++ src/Components/Profile/MyPosts/Textarea.tsx
@@ -0,0 +1,34 @@
+import React, {ChangeEvent, KeyboardEvent} from "react";
+import styles from "./MyPosts.module.css";
+
+type TextareaPropsType = {
+    error: boolean
+    setError: (error: boolean) => void
+    newPostText: string
+    onAddNewPost: () => void
+    updateNewPostText: (text: string) => void
+}
+
+export const TextareaForMyPosts = (props: TextareaPropsType) => {
+
+    const onPostChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        props.updateNewPostText(event.currentTarget.value)
+        props.setError(false)
+    }
+
+    const onKeyPress = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === 'Enter') props.onAddNewPost()
+    }
+
+
+    return (
+        <div>
+            <textarea value={props.newPostText}
+                      placeholder={'enter your message'}
+                      onChange={onPostChange}
+                      onKeyPress={onKeyPress}
+                      className={props.error ? styles.error : ''}
+            />
+        </div>
+    )
+}
